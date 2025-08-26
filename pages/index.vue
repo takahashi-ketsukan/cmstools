87c51e253021e0436e1177c730e7970af1b3403e
ketsukan-takahashi <ketsukan-takahashi@uchida.co.jp>
2025-08-26 10:07:31 +0900

    p2

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,16 @@
         <div>{{ response.details.ext_2 }}</div>
         <div>{{ response.details.ext_3 }}</div>
 
-        <div v-for="n in response.details.ext_4" :key="n.slag">
-            <img :src="n.ext_4.url" width="400">
-            <div>{{ n.ext_3 }}</div>
-            <div>{{ n.ext_5 }}</div>
-            <div>{{ n.ext_6 }}</div>
-        </div>
     </div>
+    <hr>
+    <h1>test</h1>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 
 const { data: response } = await useFetch(
-    `${config.public.apiBase}/rcms-api/3/service/6`,
+    `${config.public.apiBase}/rcms-api/3/service/7`,
     {
         credentials: 'include',
     }

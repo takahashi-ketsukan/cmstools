95e72e7fb4cdf2657c9cc8e64c86388dc0108dd4
ketsukan-takahashi <ketsukan-takahashi@uchida.co.jp>
2025-08-26 14:54:53 +0900

    3

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,10 @@
 <template>
-  <div v-if="response">
-    <h2>{{ response.details.subject }}</h2>
-    <img :src="response.details.ext_1?.url" width="800" />
-    <div>{{ response.details.ext_2 }}</div>
-    <div>
-      <a :href="response.details.ext_3?.url" target="_blank">
-        ファイルをダウンロード
-      </a>
-    </div>
-  </div>
-  <div v-else>
-    読み込み中...
+  <div>
+    <h1>テストページ</h1>
+    <p>ここが表示されれば Nuxt のテンプレートOK</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue"
-
-const config = useRuntimeConfig()
-const response = ref<any>(null)
-
-onMounted(async () => {
-  const { data, error } = await useFetch(
-    `${config.public.apiBase}/rcms-api/3/service/7`
-  )
-  response.value = data.value
-  console.log("API Response:", data.value)
-  console.log("Error:", error.value)
-})
+console.log("index.vue 読み込まれた")
 </script>
